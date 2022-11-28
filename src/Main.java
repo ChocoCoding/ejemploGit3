9a842f4ae2891ae9a1fc5815c61f1f69eb2dbf80
Gonzalo Campos Dominguez <gonzalo.dominguezcd@gmail.com>
2022-11-28 21:09:58 +0100

    añadiendo código para probar las propiedades de Java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@
  * @Date: ${DATE}
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Empleado p = new Empleado("Pepe Pérez", 30);
+        System.out.println(p);
+
+        p.nombre = "Otro nombre";
+        System.out.println(p);
+
     }
+
 }
